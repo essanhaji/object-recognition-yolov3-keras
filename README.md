217fc01b9dab79d449b683b00ccd6d60d1b6d9fe
El Houcine ES SANHAJI <elhoucineessanhaji@gmail.com>
2021-01-01 16:22:01 +0100

    addin classes

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
-# object-recognition-yolov3-keras
+# Object Recognition With YOLOv3 in Keras
+
+## Summary
+
+Object recognition with YOLOv3 for Keras users based on [keras yolo3 GitHub repository](https://github.com/experiencor/keras-yolo3).
+
+<br>
+
+## Requirements
+
+- Get the package manager **PyPi** ready.
+
+- All requirements that you will need with its version it's exist in `requirements.txt` so you need just to run this command to install it all :
+
+
+   ```
+   !pip install -r requirements.txt
+   ```
+
+- Get the YOLOv3 pretrained weights from [here](https://pjreddie.com/media/files/yolov3.weights) and add it to the repository.
+
+
+<br>
+
+## How to run it
+
+- When you install all requirements pick in the repository and open the terminal then :
+
+   ```
+   python build_model.py
+   ```
+   to encode the `YOLOv3` model format to `Keras` model format.
+
+<br>
+
+## Authors
+
+* [El Houcine ES SANHAJI](https://www.linkedin.com/in/essanhaji/)
+
+<br>
+
+## Thank you.
--- tools.py
+++ tools.py
@@ -262,3 +262,17 @@ def draw_boxes(filename, v_boxes, v_labels, v_scores):
 		pyplot.text(x1, y1, label, color='white')
 	# show the plot
 	pyplot.show()
+
+
+l = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck",
+	"boat", "traffic light", "fire hydrant", "stop sign", "parking meter", "bench",
+	"bird", "cat", "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe",
+	"backpack", "umbrella", "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard",
+	"sports ball", "kite", "baseball bat", "baseball glove", "skateboard", "surfboard",
+	"tennis racket", "bottle", "wine glass", "cup", "fork", "knife", "spoon", "bowl", "banana",
+	"apple", "sandwich", "orange", "broccoli", "carrot", "hot dog", "pizza", "donut", "cake",
+	"chair", "sofa", "pottedplant", "bed", "diningtable", "toilet", "tvmonitor", "laptop", "mouse",
+	"remote", "keyboard", "cell phone", "microwave", "oven", "toaster", "sink", "refrigerator",
+	"book", "clock", "vase", "scissors", "teddy bear", "hair drier", "toothbrush"]
+
+print("\n".join(l))
